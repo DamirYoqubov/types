032097fe79d83e181c5e6a03d1a936bfb219d319
DamirYoqubov <damiryoqubov@gmail.com>
2023-02-06 15:26:15 +0900

    добавлена функция Withdraw

---
+++ pkg/bank/card/card.go
@@ -0,0 +1,22 @@
+package card
+
+import "github.com/DamirYoqubov/types/pkg/bank/types"
+
+// Withdraw снимает деньги с карты
+func Withdraw(card *types.Card, amount types.Money)  {
+	const withdrawLimit = 20_000_00
+	if amount < 0 {
+		return
+	}
+	if amount > withdrawLimit {
+		return
+	} 
+	if !card.Active {
+		return
+	}
+	if card.Balance < 0 {
+		return
+	}
+
+	card.Balance -= amount
+}
